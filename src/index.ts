af77d8d8a048db94eafd113c45a4bbd860cffd8a
凉菜 <1659488338@qq.com>
2022-05-06 11:52:41 +0800

    package-lock

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,7 @@ import {createApp} from "oitq";
 import * as httpServer from '@oitq/plugin-http-server'
 import * as oneBot from '@oitq/plugin-one-bot'
 
-const app = createApp({logLevel: 'info'})
-app.plugin(httpServer, {port: 8086}).plugin(oneBot)
-app.addBot({
-    uin: 1234567890,
-    password: '*********',
-    type: "password",
-    config: {platform: 5},
-    oneBot: true
-})
-app.start()
+createApp({logLevel: 'info'})
+    .plugin(httpServer, {port: 8086})
+    .plugin(oneBot)
+    .start()
